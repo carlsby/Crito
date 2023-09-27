76bc066ce71d842a40ea17609b1050c8b7b87241
carlsby <carlandersson99@outlook.com>
2023-09-27 15:25:43 +0200

    changes

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,39 @@
+﻿using Crito.Models;
+using Crito.Services;
+using MailKit;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Crito.Controllers;
+
+public class ContactsController : SurfaceController
+{
+    public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+    {
+    }
+
+    [HttpPost]
+    public async Task <IActionResult> Index(ContactForm contactform)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = "Form was not submitted.";
+        }
+        else
+        {
+            TempData["SuccessMessage"] = "Form submitted successfully!";
+            using var mail = new EmailService("carlandersson99@outlook.com", "smtp-mail.outlook.com", 587, "carlandersson99@outlook.com", "");
+
+            await mail.SendAsync(contactform.Email, "Your request was received.", "Hi, your request was received and we will be in contact with you as soon as possible.");
+            await mail.SendAsync("carlandersson99@outlook.com", $"{contactform.Name} has sent a contact request.", contactform.Message).ConfigureAwait(false);
+        }
+
+        return LocalRedirect(contactform.RedirectUrl ?? "/contact");
+    }
+}
---
+++ Services/EmailService.cs
@@ -0,0 +1,62 @@
+﻿using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+using static Lucene.Net.Index.TermsEnum;
+
+namespace Crito.Services;
+
+public class EmailService : IDisposable
+{
+    private string _from;
+    private string _smtp;
+    private int _port;
+    private string _username;
+    private string _password;
+    private MailKit.Net.Smtp.SmtpClient _client;
+
+    public EmailService(string from, string smtp, int port, string username, string password)
+    {
+        _from = from;
+        _smtp = smtp;
+        _port = port;
+        _username = username;
+        _password = password;
+        _client = new MailKit.Net.Smtp.SmtpClient();
+    }
+
+    public async Task SendAsync(string to, string subject, string body)
+    {
+
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse(_from));
+        email.To.Add(MailboxAddress.Parse(to));
+        email.Subject = subject;
+        email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+        await _client.ConnectAsync(_smtp, _port, SecureSocketOptions.StartTls);
+        await _client.AuthenticateAsync(_username, _password);
+
+        await _client.SendAsync(email);
+        await _client.DisconnectAsync(true);
+
+    }
+
+
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _client.DisconnectAsync(true).ConfigureAwait(false);
+        }
+    }
+}
